2c158e54dc8c07579597a949e50b526b8ae4fad2
omril <omril@rapidapi.com>
2019-08-25 10:18:04 +0300

    first commit

--- index.js
+++ index.js
@@ -7,17 +7,15 @@ app.get("/say_hello", (req, res, next) => {
 
 app.get("/", (req, res, next) =>{
 var o = {} // empty Object
-var key = 'Orientation Sensor';
+var key = 'My API';
 o[key] = []; // empty Array, which you can push() values into
 
 
 var data = {
-    sampleTime: '1450632410296',
-    data: '76.36731:3.4651554:0.5665419'
+    message: 'hello world'
 };
 var data2 = {
-    sampleTime: '1450632410296',
-    data: '78.15431:0.5247617:-0.20050584'
+    foo: 'bar'
 };
 o[key].push(data);
 o[key].push(data2);
